fb00b9529f4e09126ac64785507defcf74e93907
adenjwong <aden.j.wong@gmail.com>
2026-04-08 10:26:01 -0400

    create inspect images logic

---
+++ src/inspect_images.py
@@ -0,0 +1,22 @@
+import SimpleITK as sitk
+import argparse
+
+def print_image_info(name, img):
+    print(f"{name}:")
+    print("  Size:", img.GetSize())
+    print("  Spacing:", img.GetSpacing())
+    print("  Origin:", img.GetOrigin())
+    print("  Direction:", img.GetDirection())
+    print("  PixelID:", img.GetPixelIDTypeAsString())
+    print()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ct", required=True)
+parser.add_argument("--mri", required=True)
+args = parser.parse_args()
+
+ct = sitk.ReadImage(args.ct)
+mri = sitk.ReadImage(args.mri)
+
+print_image_info("CT", ct)
+print_image_info("MRI", mri)
